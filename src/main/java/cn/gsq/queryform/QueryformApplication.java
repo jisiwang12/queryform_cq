e24fb17d60709b849004f5bd6776403f40fe0ea8
jisiwang12 <qsefr@qq.com>
2021-02-25 16:33:07 +0800

    Initial commit

---
+++ src/main/java/cn/gsq/queryform/QueryformApplication.java
@@ -0,0 +1,14 @@
+package cn.gsq.queryform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication()
+public class QueryformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/gsq/queryform/QueryformApplicationTests.java
@@ -0,0 +1,19 @@
+package cn.gsq.queryform;
+
+import cn.gsq.queryform.controller.SCDDController;
+import cn.gsq.queryform.domain.SCDD;
+import cn.gsq.queryform.service.impl.SCDDServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class QueryformApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
